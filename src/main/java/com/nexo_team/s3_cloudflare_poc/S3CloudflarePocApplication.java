e990c56a55b913bd6fe8f947aeb1db22bc3a72a3
Fedesan14 <fede14.sanchez@gmail.com>
2026-05-30 14:10:55 -0300

    Initial commit

---
+++ src/main/java/com/nexo_team/s3_cloudflare_poc/S3CloudflarePocApplication.java
@@ -0,0 +1,13 @@
+package com.nexo_team.s3_cloudflare_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3CloudflarePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S3CloudflarePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexo_team/s3_cloudflare_poc/S3CloudflarePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexo_team.s3_cloudflare_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S3CloudflarePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
